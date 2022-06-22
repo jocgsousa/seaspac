6324ceece313f60f4ca6bb5528c0779d1a8c4229
jocgsousa <gregoriociacom@gmail.com>
2022-06-21 23:44:55 -0300

    APp updated

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { RiSettings3Fill } from "react-icons/ri";
 import PropTypes from "prop-types";
+import { ClassicSpinner } from "react-spinners-kit";
+import axios from "axios";
 import {
   Container,
   Form,
@@ -16,20 +18,64 @@ import {
 
 export default class Login extends Component {
   state = {
-    usuario: null,
+    username: null,
     senha: null,
     api: "",
+    loading: false,
+    errorApi: false,
   };
 
   componentDidMount() {}
 
-  handleLogin = (event) => {
+  handleLogin = async (event) => {
+    console.log("login acionado");
+
+    const { page } = this.props;
     event.preventDefault();
-    alert("Login acionado");
+
+    // GET API
+    const api = localStorage.getItem("api");
+    const { username, password } = this.state;
+    if (api) {
+      this.setState({
+        loading: true,
+      });
+      // REQUEST - POST - SESSION
+      const data = {
+        username,
+        password,
+      };
+
+      await axios
+        .post(`${api.url}/session`, data)
+        .then((response) => {
+          localStorage.setItem("credentials", JSON.stringify(response.data));
+
+          page({
+            login: false,
+            home: true,
+            config: false,
+          });
+        })
+        .catch((err) => {
+          if (err.response.data) {
+            window.alert(err.response.data);
+          } else {
+            window.alert("Falha de conexão");
+          }
+        });
+      this.setState({
+        loading: false,
+      });
+    } else {
+      this.setState({
+        errorApi: true,
+      });
+    }
   };
 
   render() {
-    const { usuario, senha } = this.state;
+    const { username, senha, loading, errorApi } = this.state;
     const { page } = this.props;
 
     return (
@@ -46,30 +92,36 @@ export default class Login extends Component {
         >
           <RiSettings3Fill size={20} />
         </ButtonConfig>
-        <Form onSubmit={this.handleLogin}>
-          <HeaderForm>
-            <h2>LOGIN</h2>
-          </HeaderForm>
-          <BodyForm>
-            <Input
-              type="text"
-              required
-              placeholder="Usuário"
-              value={usuario}
-              onChange={(e) => this.setState({ usuario: e.target.value })}
-            />
-            <Input
-              type="password"
-              required
-              placeholder="Senha"
-              value={senha}
-              onChange={(e) => this.setState({ senha: e.target.value })}
-            />
-          </BodyForm>
-          <FooterForm>
-            <ButtonSubmit type="submit">Entrar</ButtonSubmit>
-          </FooterForm>
-        </Form>
+
+        {loading ? (
+          <ClassicSpinner size={40} color="white" />
+        ) : (
+          <Form onSubmit={this.handleLogin}>
+            <HeaderForm>
+              <h2>LOGIN</h2>
+            </HeaderForm>
+            <BodyForm>
+              <Input
+                type="text"
+                required
+                placeholder="Usuário"
+                value={username}
+                onChange={(e) => this.setState({ username: e.target.value })}
+              />
+              <Input
+                type="password"
+                required
+                placeholder="Senha"
+                value={senha}
+                onChange={(e) => this.setState({ senha: e.target.value })}
+              />
+            </BodyForm>
+            <FooterForm>
+              <ButtonSubmit type="submit">Entrar</ButtonSubmit>
+              {errorApi && <h3>SEM API</h3>}
+            </FooterForm>
+          </Form>
+        )}
       </Container>
     );
   }
